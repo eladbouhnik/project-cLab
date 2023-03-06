75b8e49ac21de34a0825abf466fe632bc7419807
silverGolem <123875796+silverGolem@users.noreply.github.com>
2023-03-06 11:07:07 +0200

    Update assemblerR.c

--- pass1/assemblerR.c
+++ pass1/assemblerR.c
@@ -1,19 +1,3 @@
-/*
-	this program compile ".as" files and writes to the standard output
-	the errors if there are any.
-	
-	the errors that are reported, reffering to lines in the created ".am" file (unless written otherwise).
-	
-	if the compilation went successfully, ".ent", ".ext" and ".obj" files will be created.
-	the ".ent" file is a list of labels declared in the current file that are aloowed to use
-	in other files (it is posiable using the ".entry" guide).
-	the ".ext" file is a list of labels from other files that are used in the current
-	file (to import a label from other file, use the ".extern" guide).
-	the ".obj" file is the original program and data created by the guides, after convertion to codes
-	in base 32 characters representing them.
-	
-	authors: Elon Adam, Yoav Sayag. <3
-*/
 
 
 #include <stdio.h>
