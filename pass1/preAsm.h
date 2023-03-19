08fb8a573a1d6b4eaee28e6851a7bfc83c22d550
silverGolem <123875796+silverGolem@users.noreply.github.com>
2023-03-19 14:33:03 +0200

    Delete pass1 directory

--- pass1/preAsm.h
+++
@@ -1,9 +0,0 @@
-
-/* header for the macros of the pre-assembler functions	*/
-#include <stdio.h>
-#include<stdlib.h>
-#include <string.h>
-#define MAC "mcr" /* the macro declaration keyword */
-#define EMAC "endmcr" /* end of macro declaration keyword */
-
-
