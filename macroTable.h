e74ebf9760fec108d21187f951d58109f13d72c7
eladbouhnik <124210546+eladbouhnik@users.noreply.github.com>
2023-03-23 16:37:55 +0200

    Update macroTable.h

--- macroTable.h
+++ macroTable.h
@@ -7,11 +7,12 @@ typedef struct macro{
 
 	/*get a string and check if it is a valid string declaration:
 	check if the string is of the following format "macro NAME".
-	the NAME is not a label, instruction, guide or register.
+	The NAME is not a label, instruction, guide or register.
 	there must be BLK only between every two "fields" and there
-	can't be consectutive BLK.
+	can't be consecutive BLK.
 	if it is a valid declaration, return a pointer to the start of the
 	name in the string. otherwise, return NULL.*/
+
 
 char *ismacrodec(newMacro *head, char *str);
 int addMcr(newMacro *head,char *newName,char *newContent);
