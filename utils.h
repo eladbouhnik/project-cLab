4570eaae72850a7fd2716b30c3d50865c621cd7d
eladbouhnik <124210546+eladbouhnik@users.noreply.github.com>
2023-03-23 16:59:20 +0200

    Update utils.h

--- utils.h
+++ utils.h
@@ -22,8 +22,8 @@
 /* adds 2 strings together, free the previous str1 and put the new string in str1 */
 #define addfree(str1, str2) {										\
 								char *temp = addstr(str1, str2);	\
-								free(str1);							\
-								str1 = temp;						\
+								free(str1);				\
+								str1 = temp;				\
 							}
 							
 /* get a string of "WORDSIZE" length and move all the characters to the left to make place for the ARE bits (the characters from position 0 to ARELEN-1 are discarded) */
@@ -36,7 +36,8 @@ extern char *regs[]; /* declar the register array */
  	
 /*	duplicate the first field of the given string (pls free the memory with
 	the "free" function from "stdlib.h").
-	if couldn't allocate memory for the new string, return NULL.	*/
+	if couldn't allocate memory for the new string, return NULL.	
+*/
 char *dupl(char *str);
 
 
@@ -44,25 +45,33 @@ char *dupl(char *str);
 	is the str is NULL, the function return an empty
 	string (that should be freed later).
 	free the memory with the "free" function from "stdlib.h".
-	if couldn't allocate memory for the new string, return NULL.	*/
+	if couldn't allocate memory for the new string, return NULL.	
+*/
+
 char *duplw(char *str);
 
 
 /*	duplicate the  given string (pls free the memmory with
 	the "free" function from "stdlin.h").
-	if couldn't allocate memmory for the new string, return NULL.	*/
+	if couldn't allocate memmory for the new string, return NULL.
+*/
+
 char *duplstr(char *str);
 
 
 /*	skip field in given line.
 	assume there is only one BLK between fields and nowhere else.
 	return pointer to the start of the next field.
-	if there is only one field, return pointer to the end of the string (EOS) */
+	if there is only one field, return pointer to the end of the string (EOS) 
+*/
+
 char *skipfd(char *str);
 
 
 /*	count chars until COM, BLK or EOS
-	return the length of the field.		*/			
+	return the length of the field.		
+*/
+
 int sizeoffd(char *str);
 
 
@@ -86,7 +95,9 @@ int sizeoffd(char *str);
 		GTYP			finished successfully and found guide type.
 		ITYP			finished successfully and found instruction type.
 	warnning
-		the function changes the content of the given string.           */
+		the function changes the content of the given string.          
+*/
+
 int splitline(char *line, void **command, char * **args, int *lenargs);
 
 
@@ -94,7 +105,9 @@ int splitline(char *line, void **command, char * **args, int *lenargs);
 	return NULL if couldn't allocate memory for the
 	new string.
 	when finished using the string call the "free"
-	function from stdlib.h in order to free the memory.		*/
+	function from stdlib.h in order to free the memory.		
+*/
+
 char *addstr(char *, char *);
 
 
@@ -102,56 +115,68 @@ char *addstr(char *, char *);
 /*	check if given string is a register.
 	search legal register name syntax.
 	returns NULL if illegal name.
-	return pointer to string that equal to the name of the register. */
+	return pointer to string that equal to the name of the register.
+*/
+
 char *isreg(char *str);
 
 /*	get int and returns its binary form (in two's compliment) as a string.
 	the string has 10 characters and any bit that is after the tenth bit
 	will be ignored.
-	if couldn't allocate memory, return NULL.		*/
+	if couldn't allocate memory, return NULL.		
+*/
+
 char *itostr(int num); 
 
 
 /*	search for addressing method for given operand
 	based on the operand's syntax.
 	return the legal addresing method (AC0, AC1, AC2, AC3)
-	or ERR_OPD if illegel operand.				*/
+	or ERR_OPD if illegel operand.			
+*/
 int findadr(char *str);
 
 
 /* checks whether the operand is in the format of an operand of addressing method 2*/
+
 int isAdr2(char *str);
-
-
 
 /*	check if given string is label.
 	check for forbidden names as reg, guide or instruction name.
 	return null if use forbidden name.
-	return ptr to string that is a copy of the label.		*/						
+	return ptr to string that is a copy of the label.		
+*/
+
 char *islbl (char *line);
 
 /*	return x to the power of y.
-	if y is non-positive, return 1.	*/
+	if y is non-positive, return 1.
+*/
+
 int powr(int x, int y);
 
 /*	creates a array of strings So that we can mess with the values inside the brackets	*/
+
 char** getParams(char *str);
 
-
 /*	removing all of the white spaces in the string	*/
-int removeWhiteSpace (char *str);
 
+int removeWhiteSpace (char *str);
 
 /*	convert string that represent binary number to special base 2.
 	assume the string has 14 characters.
 	return NULL if can't allocate memory.
-	return pointer to the converted string if suceeded.			*/
+	return pointer to the converted string if suceeded.		
+*/
+
 char *convert2(char *inputStr);
 
 
 /*	free all given pointers.
 	pointers has to point to memory allocated by calloc\malloc.						
-	the last parameters has to be NULL.				*/
+	the last parameters has to be NULL.				
+*/
+
 void freeall (void * ptr, ...);
 
 
@@ -160,7 +185,9 @@ void freeall (void * ptr, ...);
 	even if couldn't close a file, the function 
 	will try to close the next FILE pointers.
 	return SUCC if succeded to close all
-	and ERR_FCE if couldn't. 			*/
+	and ERR_FCE if couldn't. 			
+*/
+
 int fcloseall (FILE * ptr, ...);
 void trimEnd(char *str);
 
