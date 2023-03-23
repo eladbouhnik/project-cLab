82c5d6106f7b834486eea3e936bf1cb72b834114
eladbouhnik <124210546+eladbouhnik@users.noreply.github.com>
2023-03-23 16:26:37 +0200

    Update guides.h

--- guides.h
+++ guides.h
@@ -5,9 +5,6 @@
 #ifndef GUIDES
 #define GUIDES
 
-
-
-
 /* for the data allocation guides which has integers has operands (struct and data) */
 #define MAXDTNUM 8191  /* the highest signed integer that can be held in 14 bits: 2^9 - 1 */
 #define MINDTNUM -8192  /* the lowest signed integer that can be held in 14 bits: -2^13 */
@@ -20,15 +17,15 @@
 #define GNM -1 /* no maximum number of operands */
 
 #define GNDEF 0 /* the guide doesn't have a default type for all the operands or the array isn't used*/
-#define STRTYP 1 /* the operand is string. */
-#define INTTYP 2 /* the operand is integer. */
-#define LBLTYP 3 /* the operand is label. */
+#define STRTYP 1 /* The operand is string. */
+#define INTTYP 2 /* The operand is integer. */
+#define LBLTYP 3 /* The operand is label. */
 
 #define GALL 1  /* the guide is a data (numbers or string) allocation, and also operate as a flag in the guide's kind field */
 #define GEXT 2  /* the guide is a declaration about a label from other file */
 #define GENT 4  /* the guide is a declaration that other guides can use a label */
 
-#define GTYP -2 /* flag tp identify guide */
+#define GTYP -2 /* flag to identify guide */
 
 
 /* define guide and make an array of them, sorted alphabetically by their names */
@@ -48,10 +45,12 @@ extern guide guids[]; /* array of the guides */
 	/*function that perform binary search to look for guide and
 	in the "guids".
 	assume they are sorted alphabetically by names.
-	return a pointer to the ones with matching name.
-	if couldn't find any, return NULL.*/
+	return a pointer to the ones with matching names.
+	if It couldn't find any, return NULL.*/
 
 guide *findguide(char *name);
 
 #endif
+
+
 
