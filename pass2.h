6825b203b95edf8bd219429c838a497e559c2cc1
silverGolem <123875796+silverGolem@users.noreply.github.com>
2023-03-24 18:08:02 +0300

    Update pass2.h

--- pass2.h
+++ pass2.h
@@ -2,3 +2,6 @@
 
 #define fstArg args[0] /* the first argument of the line */
 #define secArg args[1] /* the second argument of the line */
+#define fstPar params[0] /* the first parameter of the operand */
+#define secPar params[1] /* the second parameter of the operand  */
+
